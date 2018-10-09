641317fa7949394485cf3399be33a50a6015c15c
angiecortez <angiecorteztay1@hotmail.com>
2018-10-09 12:16:15 -0500

    haciendo deploy, faltan estilos

--- lab-songs-frameworks/config/index.js
+++ lab-songs-frameworks/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/lim-2018-01-lab-songs-frameworks',
 
     /**
      * Source Maps
